bca5909d2f6bf6d9a1552972d795b3ea651e16bb
José Guillardi <63321040+JoseGu1llardi@users.noreply.github.com>
2024-12-16 18:23:23 +0000

    feat: creating entities

---
+++ src/main/java/com/techevents/api/domain/coupon/Coupon.java
@@ -0,0 +1,32 @@
+package com.techevents.api.domain.coupon;
+
+import com.techevents.api.domain.event.Event;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Table(name = "coupon")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Coupon {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}
